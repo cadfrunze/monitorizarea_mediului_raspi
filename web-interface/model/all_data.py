f17caa77e409562ef9dc619a306adf17f49126a8
Marius <marius1890@yahoo.com>
2025-05-31 22:45:12 +0300

    Refactor AllData class to use a single DbAccess instance and update data_range method parameters for consistency

--- web-interface/model/all_data.py
+++ web-interface/model/all_data.py
@@ -1,20 +1,23 @@
 from databases.db_access import DbAccess
 
 
+db: DbAccess = DbAccess()
+
 class AllData:
     """
     Clasa AllData este responsabila pentru gestionarea datelor din baza de date.
     """
     def __init__(self):
-        self.__db: DbAccess = DbAccess()
+
+        self.all_data: dict = db.get_data()
     
     
     def data_range(self, start_hour:int, end_hour:int, ziua1: str, ziua2:str) -> dict | None:
         """
         Returneaza intervalul de date din baza de date.
         """
-        all_data = self.__db.get_data()
-        if all_data is None:
+        
+        if self.all_data is None:
             raise Exception("Nu s-au putut obtine datele din baza de date.")
         new_data: dict = {
             key: {
@@ -22,7 +25,7 @@ class AllData:
                 for sub_key, sub_val in values.items()
                 if (start_hour <= sub_val['hour'] <= end_hour) and ziua1 == sub_val['day'] and ziua2 == sub_val['day']
                 }
-            for key, values in all_data.items()
+            for key, values in self.all_data.items()
         }
         return new_data
 
