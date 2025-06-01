8f288e154a6074c4e08346e9f19d1b0dd17de3e1
Marius <marius1890@yahoo.com>
2025-06-01 14:42:12 +0300

    Update data_range method to handle date intervals and adjust testing parameters

--- web-interface/model/all_data.py
+++ web-interface/model/all_data.py
@@ -1,5 +1,5 @@
 from databases.db_access import DbAccess
-
+from datetime import datetime, timedelta
 
 db: DbAccess = DbAccess()
 
@@ -19,19 +19,46 @@ class AllData:
         
         if self.all_data is None:
             raise Exception("Nu s-au putut obtine datele din baza de date.")
-        new_data: dict = {
-            key: {
-                sub_key: sub_val
-                for sub_key, sub_val in values.items()
-                if (start_hour <= sub_val['hour'] <= end_hour) and ziua1 == sub_val['day'] and ziua2 == sub_val['day']
-                }
-            for key, values in self.all_data.items()
-        }
-        return new_data
-
-        
-  
-        
+        # Obtinerea intervalului de zile
+        final_data: dict[str, list[tuple[int, float]]] = {
+            "temp": [],
+            "hum": [],
+            "press": [],
+            }
+        interval_time: list[list[str, list[int]]] = list() # index[0] = ziua, index[1] = intervalul de ore
+        interval_hours: list[int] = list()
+        start_date: datetime = datetime.strptime(ziua1, "%d-%m-%Y")
+        end_date: datetime = datetime.strptime(ziua2, "%d-%m-%Y")
+        current_date: datetime = start_date
+        while current_date <= end_date:
+            interval_time.append([current_date.strftime("%d-%m-%Y")])
+            current_date += timedelta(days=1)
+        # Obtinerea intervalului de ore
+        if len(interval_time) == 1:
+            interval_hours = [ora for ora in range(start_hour, end_hour + 1)]
+            interval_time.append(interval_hours)
+        else:
+            for day in interval_time:
+                if day == interval_time[0]:
+                    day.append([ora for ora in range(start_hour, 24)])
+                elif day != interval_time[0] and day != interval_time[-1]:
+                    day.append([ora for ora in range(0, 24)])
+                elif day == interval_time[-1]:
+                    day.append([ora for ora in range(0, end_hour + 1)])
+        # Obtinerea datelor din baza de date
+        for time in interval_time:
+            for hour in time[1]: # interatia pt intervalul de ore din ziua curenta
+                for k, v in self.all_data.items():
+                    # print(f"Key: {k}, Value: {v}")
+                    for data in v.values():
+                        if data["day"] == time[0] and data["hour"] == hour:
+                            if k == "temp":
+                                final_data["temp"].append((hour, data["value_temp"]))
+                            elif k == "hum":
+                                final_data["hum"].append((hour, data["value_hum"]))
+                            elif k == "press":
+                                final_data["press"].append((hour, data["value_press"]))
+        return final_data
         
         
         
--- web-interface/testing.py
+++ web-interface/testing.py
@@ -1,4 +1,4 @@
 from model.all_data import AllData
 
 all_data: AllData = AllData()
-print(all_data.data_range(21, 21, "31-05-2025", "31-05-2025"))  # For testing purposes, prints the data range from the database
+print(all_data.data_range(21, 3, "31-05-2025", "01-06-2025"))  # For testing purposes, prints the data range from the database
