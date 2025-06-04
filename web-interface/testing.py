08aba08979db7760eca2ec1330d59bad38732f60
Marius <marius1890@yahoo.com>
2025-06-04 23:10:35 +0300

    #

--- web-interface/testing.py
+++ web-interface/testing.py
@@ -5,7 +5,7 @@
 # # all_data: AllData = AllData()
 # # print(all_data.data_range(21, 3, "31-05-2025", "01-06-2025"))  # For testing purposes, prints the data range from the database
 # # print(len(all_data.data_range(21, 3, "31-05-2025", "01-06-2025")["temp"]))
-from model.raspi import RaspiSsh
+# from model.raspi import RaspiSsh
 # app_service = AppService()
 # app_service.get_all_data(21, 3, "31-05-2025", "01-06-2025")
 
@@ -21,6 +21,8 @@ from model.raspi import RaspiSsh
 # a = get_info_raspi()
 # print(a["ip"])  # Prints the IP and SSID of the Raspberry Pi
 
-raspi: RaspiSsh = RaspiSsh()
+# raspi: RaspiSsh = RaspiSsh()
 
-raspi.run_script()
+# raspi.run_script()
+
+print(type(int))
